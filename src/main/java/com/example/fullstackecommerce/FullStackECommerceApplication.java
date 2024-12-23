9ff840318c9c7d420ebb5b1223c62c65dcc2de80
harshb03 <harshbuddhdev395@gmail.com>
2024-12-23 21:21:53 +0530

    first commit

---
+++ src/main/java/com/example/fullstackecommerce/FullStackECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackecommerce/FullStackECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
